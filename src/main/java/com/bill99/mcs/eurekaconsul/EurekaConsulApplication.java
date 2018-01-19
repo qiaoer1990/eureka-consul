ee89546e66d900d852ebf01962cff0d7eaebec7d
qiaoer <767907320@qq.com>
2018-01-19 17:37:55 +0800

    consul

---
+++ src/main/java/com/bill99/mcs/eurekaconsul/EurekaConsulApplication.java
@@ -0,0 +1,12 @@
+package com.bill99.mcs.eurekaconsul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaConsulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConsulApplication.class, args);
+    }
+}
---
+++ src/test/java/com/bill99/mcs/eurekaconsul/EurekaConsulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bill99.mcs.eurekaconsul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaConsulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
